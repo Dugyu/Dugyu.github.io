ebe9d2623e7356b951efddfc1aaff31381970fb2
Dugyu <yxylyzy@126.com>
2019-12-24 20:55:48 -0800

    finish catalog project class definition

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
    });
+
+
+projectList = [];
+
+queue()
+    .defer(d3.csv, "data/catalog.csv")
+    .await(createProjectCatalog)
+
+function createProjectCatalog(error, catalogData){
+    catalogData.forEach(e => {
+    var project = new Project("catalog",e);
+    projectList.push(project);
+   });
+}
+
--- js/project.js
+++ js/project.js
@@ -8,16 +8,44 @@ class Project{
 
     parentEleId;
     data;
+    parentElement;
 
     constructor(_parentEleId,_data)
     {
         this.parentEleId = _parentEleId;
         this.data = _data;
+        this.parentElement = document.getElementById(this.parentEleId);
+        this.initProject();
     }
 
 
     initProject(){
+        var project_wrapper = document.createElement("div");
+        project_wrapper.setAttribute("class","project-wrapper mb-5");
+        project_wrapper.setAttribute("data-aos", "fade-in");
+        
+        var cover_img = document.createElement("img");
+        cover_img.setAttribute("src", "static/"+ this.data.cover_img);
+        cover_img.setAttribute("alt",this.data.cover_img);
+        cover_img.setAttribute("class", "img-fluid project-cover");
 
+        var title = document.createElement("div");
+        title.setAttribute("class","my-5 project-title");
+        var title_text = document.createElement("h5");
+        title_text.innerHTML = this.data.title.toUpperCase();
+        title.appendChild(title_text);
+
+        var description = document.createElement("div");
+        description.setAttribute("class","project-description");
+        var description_text = document.createElement("p");
+        description_text.innerHTML = this.data.abstract;
+        description.appendChild(description_text);
+
+        project_wrapper.appendChild(cover_img);
+        project_wrapper.appendChild(title);
+        project_wrapper.appendChild(description);
+
+        this.parentElement.appendChild(project_wrapper);
     }
 
 
