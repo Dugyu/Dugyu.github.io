b409e3cdc3ec77575a1c5b756d725e6a605e137c
Dugyu <yxylyzy@126.com>
2019-12-24 17:56:01 -0800

    update cover images

---
+++ js/project.js
@@ -0,0 +1,24 @@
+/*
+ * Project - The wrapper class for project in catalog
+ * @param _parentElement 	-- the HTML element in which to render project image, title and description
+ * @param _data						-- the object that contains image name, title, description
+ */
+
+class Project{
+
+    parentEleId;
+    data;
+
+    constructor(_parentEleId,_data)
+    {
+        this.parentEleId = _parentEleId;
+        this.data = _data;
+    }
+
+
+    initProject(){
+
+    }
+
+
+}
