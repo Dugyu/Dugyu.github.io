b30d2c395e51203d61e1462213f0b856619d643b
Dugyu <yxylyzy@126.com>
2020-02-03 21:58:22 -0500

    mute video

---
+++ js/project-xenolith.js
@@ -0,0 +1,31 @@
+/*
+ * Project page  
+ */
+
+initProject();
+
+function initProject(){   
+    var siteframe = {};
+
+    queue()
+        .defer(d3.csv, "../../data/catalog.csv")
+        .defer(d3.csv, "../../data/brief.csv" )
+        .await(createProjectCatalog)
+        
+        
+    function createProjectCatalog(error, catalogData, briefData){
+        var data = catalogData;
+        data.forEach(function(d){
+            d.id = +d.id;
+        })
+        siteframe = new Siteframe(data,briefData,"catalog","../../");
+        siteframe.createContentOfCatalog();
+    }
+
+    $(window).on('load',function(){
+        console.log('loaded');
+    })
+
+
+
+}
